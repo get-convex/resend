1c24268d18b89b1595ef83386929a7a725cfc054
Jamie Turner <jamwt@convex.dev>
2025-06-13 14:53:44 -0700

    Really no react.

--- example/eslint.config.js
+++ example/eslint.config.js
@@ -1,7 +1,5 @@
 import js from "@eslint/js";
 import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -14,19 +12,9 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       // Allow explicit `any`s
       "@typescript-eslint/no-explicit-any": "off",
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": [
         "warn",
